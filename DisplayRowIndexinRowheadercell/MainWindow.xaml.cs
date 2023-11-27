48e527563dfd90f5430410b6a469f8e4c6d8945a
Sweatha.B <Sweatha.bharathi@syncfusion.com>
2023-11-27 12:36:31 +0530

    ES-857662 - KB for display the RowIndex in GridRowHeaderCell when combined with SfDatapager in WPF DataGrid

---
+++ DisplayRowIndexinRowheadercell/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Syncfusion.UI.Xaml.Controls.DataPager;
+using Syncfusion.UI.Xaml.Grid;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DisplayRowIndexinRowheadercell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            sfDataPager.PageIndexChanged += SfDataPager_PageIndexChanged;
+
+        }
+
+        private void SfDataPager_PageIndexChanged(object sender, PageIndexChangedEventArgs e)
+        {
+            for (int i = 1; i < dataGrid.RowGenerator.Items.Count; i++)
+            {
+                var rowHeaderCell = ((dataGrid.RowGenerator.Items[i] as DataRow).VisibleColumns[0] as DataColumn).ColumnElement;
+                (rowHeaderCell as GridRowHeaderCell).RowIndex = -1;
+            }
+        }
+    }
+
+    public class PageConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dataPager = values[2] as SfDataPager;
+
+            if (dataPager.PageIndex == 0 || (int)values[0] == -1)
+            {
+                return values[0].ToString();
+            }
+
+            values[0] = (int)values[0] + (((dataPager.PageIndex + 1) - 1) * dataPager.PageSize);
+            return values[0].ToString();
+
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DisplayRowIndexinRowheadercell/ViewModel/OrderInfoViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayRowIndexinRowheadercell
+{
+    public class Order : INotifyPropertyChanged
+    {
+
+
+        private ObservableCollection<OrderInfo> _orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+
+        public Order()
+        {
+            _orders = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+
+        private void GenerateOrders()
+        {
+            _orders.Add(new OrderInfo(1001, "Maria Anders", "Germany", "ALFKI", "Berlin"));
+            _orders.Add(new OrderInfo(1002, "Ana Trujilo", "Mexico", "ANATR", "Mexico D.F."));
+            _orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico D.F."));
+            _orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London"));
+            _orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula"));
+            _orders.Add(new OrderInfo(1006, "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            _orders.Add(new OrderInfo(1007, "Frederique Citeaux", "France", "BLONP", "Strasbourg"));
+            _orders.Add(new OrderInfo(1008, "Martin Sommer", "Spain", "BOLID", "Madrid"));
+            _orders.Add(new OrderInfo(1009, "Laurence Lebihan", "France", "BONAP", "Marseille"));
+            _orders.Add(new OrderInfo(1010, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen"));
+            _orders.Add(new OrderInfo(1011, "Maria Anders", "Germany", "ALFKI", "Berlin"));
+            _orders.Add(new OrderInfo(1012, "Ana Trujilo", "Mexico", "ANATR", "Mexico D.F."));
+            _orders.Add(new OrderInfo(1013, "Antonio Moreno", "Mexico", "ANTON", "Mexico D.F."));
+            _orders.Add(new OrderInfo(1014, "Thomas Hardy", "UK", "AROUT", "London"));
+            _orders.Add(new OrderInfo(1015, "Christina Berglund", "Sweden", "BERGS", "Lula"));
+            _orders.Add(new OrderInfo(1016, "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            _orders.Add(new OrderInfo(1017, "Frederique Citeaux", "France", "BLONP", "Strasbourg"));
+            _orders.Add(new OrderInfo(1018, "Martin Sommer", "Spain", "BOLID", "Madrid"));
+            _orders.Add(new OrderInfo(1019, "Laurence Lebihan", "France", "BONAP", "Marseille"));
+            _orders.Add(new OrderInfo(1020, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen"));
+
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+
+    }
+}
